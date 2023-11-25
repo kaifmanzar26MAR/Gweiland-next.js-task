7a9858f25d3d61f7af3106a16b27ee65dd15393c
Md Kaif Manzar <kaifmanzar321@gmail.com>
2023-11-25 12:30:51 +0530

    respnsivness fixing

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -15,17 +15,16 @@ const NavBar = () => {
     const toggle=()=>{
         if(NavBar==false){
             setNavBar(true);
-            document.getElementById('xxxup').style.display="flex";
-            document.getElementById('xxxdw').style.display="flex";
+            document.getElementById('main_click').style.display="flex";
+            
         }else{
             setNavBar(false);
-            document.getElementById('xxxup').style.display="none";
-            document.getElementById('xxxdw').style.display="none";
+            document.getElementById('main_click').style.display="none";
         }
     }
     const brands_fun=()=>{
         if(NavBar==true){
-            document.getElementById('br_h').style.display="flex";
+            document.getElementById('brand_click_show').style.display="flex";
         }
     }
     const shop_fun=()=>{
@@ -34,7 +33,8 @@ const NavBar = () => {
         }
     }
     const close_brand=()=>{
-        document.getElementById('br_h').style.display="none";
+        console.log("work")
+        document.getElementById('brand_click_show').style.display="none";
     }
     const back_shop=()=>{
         document.getElementById('sc').style.display="none";
@@ -55,17 +55,26 @@ const NavBar = () => {
     const close_home=()=>{
         document.getElementById('hout').style.display="none";
     }
+    
+    useEffect(()=>{
+        if(!NavBar){
+            document.getElementById('hout').style.display="none";
+            document.getElementById('apo').style.display="none";
+            document.getElementById('sc').style.display="none";
+        }
+        
+    })
    
   return (
     <>
         <nav className='w-full bg-neutral-800 top-0 left-0 right-0 z-10 sticky'>
             <div className='p-2 justify-between flex ml-10 mr-10 text-white items-center'>
-                <div className='hamburger ' onClick={()=>toggle()}>
+                <div className='hamburger ' id='hmbrg' onClick={()=>toggle()}>
                    {!NavBar ? <FaHamburger size={20} className='box'/> : <span style={{color:"red"}}>X</span>} 
                 </div>
                 
                 <div className="topnav up text-white  gap-6" id='xxxup'>
-                    <Link href="#home" className='icon_link text_hover brands' onClick={()=>brands_fun()}>brands <span className='arrow'><IoIosArrowForward size={15} /></span>
+                    <div  className='icon_link text_hover brands'>brands <span className='arrow'><IoIosArrowForward size={15} /></span>
                     <div className="hower_menue_brands" id='br_h'>
                         <ol>
                             <li><Link  href="/" className='back_brand' onClick={()=>{close_brand()}}>{'<'}- &nbsp; brands</Link></li>
@@ -80,32 +89,36 @@ const NavBar = () => {
                             <li><Link className='text_hover' href="/">vudu brigada</Link></li>
                         </ol>
                     </div>
-                    </Link>
-                    <Link href="#news" className='icon_link text_hover shop' onClick={()=>shop_fun()}>shop <span className='arrow'><IoIosArrowForward size={15} /></span>
-
-                    <div className="hower_menue_shop">
-                        <div className="apparol">
-                            <h2 className='text_hover'>apparol</h2>
-                            <ol>
-                                <li><Link className='text_hover' href="/">unisex hoodies</Link></li>
-                                <li><Link className='text_hover' href="/">unisex zip hoodies</Link></li>
-                                <li><Link className='text_hover' href="/">unisex sweatshirts</Link></li>
-                                <li><Link className='text_hover' href="/">unisex tees</Link></li>
-                                <li><Link className='text_hover' href="/">women's tanks</Link></li>
-                                <li><Link className='text_hover' href="/">unisex tankss</Link></li>
-                            </ol>
-                        </div>
-                        <div className="home">
-                        <h2 className='text_hover'>home</h2>
-                            <ol>
-                                <li><Link className='text_hover' href="/">phone case</Link></li>
-                                <li><Link className='text_hover' href="/">framed posters</Link></li>
-                                <li><Link className='text_hover' href="/">posters</Link></li>
-                            </ol>
-                        </div>
                     </div>
+
                     
-                    </Link>
+
+
+                    <div  className='icon_link text_hover shop'>shop <span className='arrow'><IoIosArrowForward size={15} /></span>
+
+                        <div className="hower_menue_shop">
+                            <div className="apparol">
+                                <h2 className='text_hover'>apparol</h2>
+                                <ol>
+                                    <li><Link className='text_hover' href="/">unisex hoodies</Link></li>
+                                    <li><Link className='text_hover' href="/">unisex zip hoodies</Link></li>
+                                    <li><Link className='text_hover' href="/">unisex sweatshirts</Link></li>
+                                    <li><Link className='text_hover' href="/">unisex tees</Link></li>
+                                    <li><Link className='text_hover' href="/">women's tanks</Link></li>
+                                    <li><Link className='text_hover' href="/">unisex tankss</Link></li>
+                                </ol>
+                            </div>
+                            <div className="home">
+                            <h2 className='text_hover'>home</h2>
+                                <ol>
+                                    <li><Link className='text_hover' href="/">phone case</Link></li>
+                                    <li><Link className='text_hover' href="/">framed posters</Link></li>
+                                    <li><Link className='text_hover' href="/">posters</Link></li>
+                                </ol>
+                            </div>
+                        </div>
+                    
+                    </div>
                     <Link href="#contact" className='icon_link text_hover'>premium</Link>
                     <Link href="#about" className='icon_link text_hover'>gift cards</Link>
                     
@@ -139,6 +152,38 @@ const NavBar = () => {
                         <li><Link className='text_hover' href="/">framed posters</Link></li>
                         <li><Link className='text_hover' href="/">posters</Link></li>
                     </ol>
+                </div>
+
+                <div id="brand_click_show">
+                    <ol>
+                        <Link  href="/" className='back_brand' onClick={()=>{close_brand()}}>{'<'}- &nbsp; brands</Link>
+                        <li><Link className='text_hover' href="/">all brands</Link></li>
+                        <li><Link className='text_hover' href="/">$hosky</Link></li>
+                        <li><Link className='text_hover' href="/">bitfins</Link></li>
+                        <li><Link className='text_hover' href="/">cardano stonerz club</Link></li>
+                        <li><Link className='text_hover' href="/">cryptoraggies</Link></li>
+                        <li><Link className='text_hover' href="/">danketsu</Link></li>
+                        <li><Link className='text_hover' href="/">future fast</Link></li>
+                        <li><Link className='text_hover' href="/">pendulum</Link></li>
+                        <li><Link className='text_hover' href="/">vudu brigada</Link></li>
+                    </ol>
+            </div>
+
+                <div id="main_click">
+                    <ol>
+                        <li className='icon_link text_hover brands' onClick={()=>brands_fun()}  style={{cursor:"pointer"}}>brands <span className='arrow'><IoIosArrowForward size={15} /></span></li>
+                        <li className='icon_link text_hover brands' onClick={()=>shop_fun()}  style={{cursor:"pointer"}}>brands <span className='arrow'><IoIosArrowForward size={15} /></span></li>
+                        
+                        <li className='icon_link text_hover'>premium</li>
+                        <li className='icon_link text_hover'>gift cards</li>
+                        <li className='icon_link text_hover'> become an affilate</li>
+                        <li><div className='flex gap-6 mt-2'>
+                            <Link href="/" className='icon_link'><FaTwitter size={20} className='icon_hover'/> </Link>
+                            <Link href="/" className='icon_link'><FaInstagram size={20} className='icon_hover'/></Link>
+                            <Link href="/" className='icon_link'><FaFacebook size={20} className='icon_hover'/></Link>
+                        </div></li>
+                    </ol>
+                    
                 </div>
                 
                 <Link href="/">
